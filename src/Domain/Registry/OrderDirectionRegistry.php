0e8e4fc6b7c6525f84b775b521127bfa590f90d3
ghriim <vignierquentin@yahoo.fr>
2021-10-27 21:50:08 +0200

    Refactoring

---
+++ src/Domain/Registry/OrderDirectionRegistry.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace COL\Librairy\BaseContracts\Domain\Registry;
+
+interface OrderDirectionRegistry
+{
+    public const ORDER_DIRECTION_ASC = 'asc';
+    public const ORDER_DIRECTION_DESC = 'desc';
+}
