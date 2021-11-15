f7ac72a877f163597a8c27b4bfd7dd4902809597
ghriim <vignierquentin@yahoo.fr>
2021-11-15 21:15:30 +0100

    Remove incorrect documentation

--- src/UseCase/GetManyUseCaseInterface.php
+++ src/UseCase/GetManyUseCaseInterface.php
@@ -2,13 +2,9 @@
 
 namespace COL\Librairy\BaseContracts\UseCase;
 
-use COL\Librairy\BaseContracts\Domain\View\Model\ViewModelInterface;
 use COL\Librairy\BaseContracts\Domain\View\Wrapper\MultipleViewModelWrapperInterface;
 
 interface GetManyUseCaseInterface
 {
-    /**
-     *  @return ViewModelInterface[]
-     */
     public function execute(string $displayFormat, array $criteria = [], ?int $pageNumber = null, ?int $nbPerPage = null): MultipleViewModelWrapperInterface;
 }
